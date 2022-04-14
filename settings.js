3e8a294025f9874e3c014579716ca83c5d26f3e4
Irfanff9 <93906910+Irfanff9@users.noreply.github.com>
2022-04-15 00:13:48 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -31,8 +31,66 @@ global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder i
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.owner = ["6289510497533","6289510497533","6285855854993"] //ganti agar fitur owner bisa di gunakan
-global.packname = 'Irpan bot' //sticker wm ubah
-global.author = 'anak emel' //sticker wm ganti nama kalian
+global.packname = 'IRFANBOT By Ipan' //sticker wm ubah
+global.author = '@IRFANFF9
+ㅤᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ
+ᅠ' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
